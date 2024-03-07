5f14b78ff94853ec9a68e27be2cab11d14ccc716
PObloj <PObloj@PIOTRO-NTB>
2024-03-07 20:45:01 +0100

    przygotowane po zmianie

--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿Console.WriteLine("Witaj mój nowy swiecie !");
-Console.WriteLine("Mam na imię Piotrek  :)");
-Console.WriteLine(" Zaczyna mi się tu podobać");
+﻿var name = "Ewa";
+var sex = "kobieta";
+var age = 31;
 
+if ( sex == "kobieta" && age <= 29)
+{
+    Console.WriteLine( "Kobieta poniżej 30 lat");
+
+}
+if ( name == "Ewa" && age == 30 )
+{
+    if ( sex == "kobieta")
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+}
+if ( sex == "męszczyzna" && age <=18 )
+{
+    Console.WriteLine("Niepełnoletni męszczyzna");
+}
