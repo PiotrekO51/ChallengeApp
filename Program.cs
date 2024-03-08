d09e27b304080e9b4ea472a0f1be9f81ada8d0d4
PObloj <PObloj@PIOTRO-NTB>
2024-03-08 09:30:51 +0100

    Warunki if else

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
-﻿var name = "Ewa";
-var sex = "kobieta";
-var age = 31;
+﻿using System;
+
+var name = "Ewa";
+var sex = "męszczyzna";
+var age = 17;
 
 if ( sex == "kobieta" && age <= 29)
 {
     Console.WriteLine( "Kobieta poniżej 30 lat");
 
 }
-if ( name == "Ewa" && age == 30 )
-{
-    if ( sex == "kobieta")
+else  if ( name == "Ewa" && age == 30 )
+
     {
         Console.WriteLine("Ewa, lat 30");
     }
-}
-if ( sex == "męszczyzna" && age <=18 )
-{
-    Console.WriteLine("Niepełnoletni męszczyzna");
-}
+else if ( sex == "męszczyzna" && age <=18 )
+
+    {
+        Console.WriteLine("Niepełnoletni męszczyzna");
+    }
